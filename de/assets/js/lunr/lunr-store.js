681cf7117e9720a066f71b88279806da321dd3d1
michelleheatherly <69756193+michelleheatherly@users.noreply.github.com>
2025-10-27 02:46:17 +0000

    Deploying to gh-pages from @ michelleheatherly/blog@b0846f73e1d092cdf85cf46e7d6fe26ce9904c4b 🚀

---
+++ de/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Frohes Neues Jahr!",
+        "excerpt":"Ich wünsche allen, die das hier lesen, dass euch das Jahr 2023 freundlich behandelt und euer Code reibungslos läuft. Wie viele andere mit Neujahrsvorsätzen ist eines meiner Ziele für dieses Jahr, an meiner persönlichen technischen Entwicklung zu arbeiten, indem ich jeden Tag programmiere. Neues Jahr, bessere Programmierung Zusätzlich zum täglichen...","categories": ["Programmierung"],
+        "tags": ["Ziele","Neues Jahr"],
+        "url": "/blog/programmierung/Frohes-Neues-Jahr/",
+        "teaser": "/blog/assets/images/placeholder-pic.jpg"
+      },{
+        "title": "Happy New Year!",
+        "excerpt":"My hope for anyone whom may be reading this, that 2023 treats you kindly and your code runs smoothly. Like many others with new years resolutions, one of my goals for this year is to work on my own personal technical development by programming each day. New Year, Better Programming...","categories": ["Programming"],
+        "tags": ["Goals","New Year"],
+        "url": "/blog/programming/Happy-New-Year/",
+        "teaser": "/blog/assets/images/placeholder-pic.jpg"
+      },{
+        "title": "Rückblick auf die Neujahrsziele",
+        "excerpt":"Es sind ein paar Monate vergangen, seit ich hier ein Update zu den beruflichen und programmierbezogenen Zielen gegeben habe, die ich mir für das neue Jahr gesetzt hatte. Mein übergeordnetes Ziel für das Jahr war es, jeden Tag zu programmieren. Das ist ein ziemlich großes Ziel – und das Leben...","categories": ["Programmierung"],
+        "tags": ["Ziele","Neues Jahr"],
+        "url": "/blog/programmierung/R%C3%BCckblick-Auf-Die-Neujahrsziele/",
+        "teaser": "/blog/assets/images/placeholder-pic.jpg"
+      },{
+        "title": "Following Up On Goals From The New Year",
+        "excerpt":"It’s been a few months since I have checked in here with the professional and programming goals I set for the New Year. My overarching goal for the year was to program every day. That’s a really big goal, and life can happen quickly and throw wrenches into achieving perfect...","categories": ["Programming"],
+        "tags": ["Goals","New Year"],
+        "url": "/blog/programming/Following-Up-On-Goals/",
+        "teaser": "/blog/assets/images/placeholder-pic.jpg"
+      }]
